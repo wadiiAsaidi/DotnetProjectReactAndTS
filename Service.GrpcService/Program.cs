2a599541095f2efd5605d23994832aa86c262ced
wadii.alsaidi@ensi-uma.tn <wadii.alsaidi@ensi-uma.tn>
2025-05-11 16:51:45 +0100

    .

--- Service.GrpcService/Program.cs
+++ Service.GrpcService/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+
 var app = builder.Build();
 
 var url = builder.Configuration.GetSection("HostingUrls:Url").Value;
